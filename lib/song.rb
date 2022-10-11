17a5e9c4763ef77cccfbe4a6669061bb470edc89
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-11 14:19:48 -0400

    commit tree dcff76f082e89fbd733cb2ce66b60ea711536681

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,19 @@
 require 'pry'
+# require_relative '../lib/concerns/memorable'
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
   def initialize
-    @@songs << self
+    super
   end
 
   def self.find_by_name(name)
@@ -18,19 +24,11 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
